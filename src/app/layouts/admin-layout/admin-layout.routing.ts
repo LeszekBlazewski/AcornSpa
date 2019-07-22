46946a152548395bbf2e2aa16b78d5c4ef6eaabd
Leszek Błażewski <blazewski.leszek@gmail.com>
2019-07-22 21:54:39 +0200

    Initial commit with blackdashboard template

---
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -0,0 +1,15 @@
+import { Routes } from "@angular/router";
+
+import { DashboardComponent } from "../../pages/dashboard/dashboard.component";
+import { IconsComponent } from "../../pages/icons/icons.component";
+import { NotificationsComponent } from "../../pages/notifications/notifications.component";
+import { UserComponent } from "../../pages/user/user.component";
+import { TablesComponent } from "../../pages/tables/tables.component";
+
+export const AdminLayoutRoutes: Routes = [
+  { path: "dashboard", component: DashboardComponent },
+  { path: "icons", component: IconsComponent },
+  { path: "notifications", component: NotificationsComponent },
+  { path: "user", component: UserComponent },
+  { path: "tables", component: TablesComponent },
+];
